d581053d1a6e282e3da4528241f0cce40222880e
Dennis de Vulder <dennisdevulder@gmail.com>
2014-01-15 23:00:09 +0100

    Refactor assert* method and add to_params

--- lib/misc/hash.rb
+++ lib/misc/hash.rb
@@ -1,9 +1,10 @@
 class Hash
   def assert_valid_keys(*valid_keys)
     valid_keys.flatten!
-    each_key do |k|
-      raise(ArgumentError, "Unknown key #{k}") unless valid_keys.include?(k)
-    end
+    each_key {|k| raise(ArgumentError, "Unknown key #{k}") unless valid_keys.include?(k)}
+  end
+
+  def to_params(namespace=nil)
+    collect{|k, v| v.to_query(namespace ? "#{namespace}[#{k}]" : k)}.sort * '&'
   end
 end
-
