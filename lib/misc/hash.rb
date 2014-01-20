b797cb2ae1804afdb4e8f4247150ba4ad36d8e17
Dennis de Vulder <dennisdevulder@gmail.com>
2014-01-21 00:38:49 +0100

    Add & sign before wrapping params

--- lib/misc/hash.rb
+++ lib/misc/hash.rb
@@ -6,7 +6,7 @@ class Hash
     each_key {|k| raise(ArgumentError, "Unknown key #{k}") unless valid_keys.include?(k)}
   end
 
-  def to_params(namespace=nil)
-    collect{|k, v| v.to_query(namespace ? "#{namespace}[#{k}]" : k)}.sort * '&'
+  def to_params
+    '&'+collect{|k, v| v.to_query(k)}.sort * '&'
   end
 end
