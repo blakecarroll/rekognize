8cbb28a4a844b74296cb8814559b301c5a1f35a3
Dennis de Vulder <dennisdevulder@gmail.com>
2014-01-20 23:57:38 +0100

    Set base logic

--- lib/rekognize/client/base.rb
+++ lib/rekognize/client/base.rb
@@ -1,17 +1,17 @@
+require File.expand_path('../methods.rb', __FILE__)
 require File.expand_path('../endpoints.rb', __FILE__)
 require File.expand_path('../jobs.rb', __FILE__)
 
 module Rekognize
   module Client
     class Base
-      attr_accessor :api_key, :api_secret
-
       include Rekognize::Client::Endpoints
       include Rekognize::Client::Jobs
 
-      def initialize(opts)
-        opts.assert_valid_keys(:api_key, :api_secret)
-        @api_key, @api_secret = opts[:api_key], opts[:api_secret]
+      mattr_reader :api_key, :api_secret
+
+      def initialize
+        true
       end
     end
 
